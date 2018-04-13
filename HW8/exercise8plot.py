7cc475a1aef3cc6dc1acfadc8d4e8cda22c871e9
tenfoldpaper <s.bien@jacobs-university.de>
2018-04-13 20:50:46 +0200

    Made the plot pretty

--- HW8/exercise8plot.py
+++ HW8/exercise8plot.py
@@ -12,4 +12,13 @@ for line in inputFile:
     MSEtest.append(float(lineList[2]))
     MCtest.append(float(lineList[3]))
 x = range(1, 241)
-plt.plot(x, MSEtrain, 'r--', x, MCtrain, 'r', x, MSEtest, 'b--', x, MCtest, 'b')
+plt.plot(x, MSEtrain, 'r--', label='Train mean square error')
+plt.plot(x, MCtrain, 'r', label='Train misclassification ratio')
+plt.plot(x, MSEtest, 'b--', label='Test mean square error')
+plt.plot(x, MCtest, 'b', label='Test misclassification ratio')
+plt.ylabel('Error')
+plt.xlabel('# of features extracted from PCA')
+
+legend = plt.legend(loc = 'upper right', shadow=False)
+
+plt.show()
