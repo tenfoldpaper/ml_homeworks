18ae18903d0531b265f29b79572a92087d3326bb
tenfoldpaper <s.bien@jacobs-university.de>
2018-04-13 21:30:35 +0200

    Added log subplot

--- HW8/exercise8plot.py
+++ HW8/exercise8plot.py
@@ -12,10 +12,17 @@ for line in inputFile:
     MSEtest.append(float(lineList[2]))
     MCtest.append(float(lineList[3]))
 
+MSEtrainLog = np.log(MSEtrain)
+MCtrainLog = np.log(MCtrain)
+MSEtestLog = np.log(MSEtest)
+MCtestLog = np.log(MCtest)
+
 x = range(1, 241)
 
 testMin = np.argmin(MSEtest)
 
+plt.figure(1)
+plt.subplot(211)
 plt.plot(x, MSEtrain, 'r--', label='Train mean square error')
 plt.plot(x, MCtrain, 'r', label='Train misclassification ratio')
 plt.plot(x, MSEtest, 'b--', label='Test mean square error')
@@ -26,5 +33,18 @@ plt.ylabel('Error')
 plt.xlabel('# of features extracted from PCA')
 
 legend = plt.legend(loc = 'upper right', shadow=False)
+
+plt.subplot(212)
+plt.plot(x, MSEtrainLog, 'r--', label='Train mean square error')
+plt.plot(x, MCtrainLog, 'r', label='Train misclassification ratio')
+plt.plot(x, MSEtestLog, 'b--', label='Test mean square error')
+plt.plot(x, MCtestLog, 'b', label='Test misclassification ratio')
+plt.annotate('test error min', xy=(testMin, MSEtestLog[testMin]), xytext=(125, 1), arrowprops=dict(facecolor='black', width=1, headwidth=3),)
+
+plt.ylabel('Error in log')
+plt.xlabel('# of features extracted from PCA')
+
+legend = plt.legend(loc = 'upper right', shadow=False)
+
 
 plt.show()
