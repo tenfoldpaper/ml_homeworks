6dd605b98cf7f30f844c8ab08bae857f3271d55b
tenfoldpaper <s.bien@jacobs-university.de>
2018-04-13 21:18:50 +0200

    Prettier plot

--- HW8/exercise8plot.py
+++ HW8/exercise8plot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 MSEtest = []
 MSEtrain = []
 MCtest = []
@@ -11,11 +11,17 @@ for line in inputFile:
     MCtrain.append(float(lineList[1]))
     MSEtest.append(float(lineList[2]))
     MCtest.append(float(lineList[3]))
+
 x = range(1, 241)
+
+testMin = np.argmin(MSEtest)
+
 plt.plot(x, MSEtrain, 'r--', label='Train mean square error')
 plt.plot(x, MCtrain, 'r', label='Train misclassification ratio')
 plt.plot(x, MSEtest, 'b--', label='Test mean square error')
 plt.plot(x, MCtest, 'b', label='Test misclassification ratio')
+plt.plot([testMin, testMin], [0, 3], 'g--')
+plt.annotate('test error min', xy=(testMin, MSEtest[testMin]), xytext=(125, 2), arrowprops=dict(facecolor='black', width=1, headwidth=3),)
 plt.ylabel('Error')
 plt.xlabel('# of features extracted from PCA')
 
