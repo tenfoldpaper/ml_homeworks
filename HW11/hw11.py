03497795fb0902fad6c650000da8d4f1b3463478
Mohit Shrestha <mo.shrestha@jacobs-university.de>
2018-05-15 00:21:04 +0200

    Everything we did in the KCO today

--- HW11/hw11.py
+++ HW11/hw11.py
@@ -1,7 +1,7 @@
 import random
 import math
 import numpy as np
-
+from numpy import linalg as LA
 random.seed()
 random.randint(0, 10)
 
@@ -15,9 +15,10 @@ def log_sig_derived(a):
 
 # eq 47
 def forward_pass_hidden_layer(W, x, n):#x = xm-1 in notes, n = length of the vector
-    #Applies sigmoid to the result 
+    #Applies sigmoid to the result
     res = (W*x)
-    temp = x
+    temp = x.copy()
+
     for i in range(0, n-1):
         temp[i] = log_sig(res[i])
     temp[n-1] = 1
@@ -42,12 +43,12 @@ def emp_risk_function(test_labelset, train_labelset):
 def update_model(model, stepsize, risk_gradient):
     return model - (stepsize*risk_gradient)
 
-# eq 55: potential of unit x 
-def x_potential(W, x_ms1, layer_size): #ms1 stands for m subtract 1 
+# eq 55: potential of unit x
+def x_potential(W, x_ms1, layer_size): #ms1 stands for m subtract 1
     W1 = W[:, 0:layer_size-1]
     x1 = x_ms1[0:layer_size-1]
     res = W1*x1
-    return res 
+    return res
 
 # eq 59
 
@@ -58,25 +59,26 @@ def loss_derived_wrt_wmij(d, x):
 def delta_output(yhat, y):
     return 2*(yhat - y)
 
+def delta_hlayer_individual(pot_at_mi, delta_next_layer, w_next_layer_toI, next_layer_size):
+    siggy = log_sig_derived(pot_at_mi)
+    sumRes = 0
+    if(next_layer_size == 1): next_layer_size += 1 #this is for the output layer, where there is no bias
+    for l in range(0, next_layer_size - 1): #next_layer_size - 1 to ignore the last component of each layer, the bias
+        sumRes += (delta_next_layer * w_next_layer_toI[l])
+    return siggy * sumRes
 # eq 63
-def delta_hlayer(pot_a_atI, delta_next_layer, w_next_layer_toI, next_layer_size):
-    res = 0 
-    siggy = log_sig_derived(pot_a_atI)
-    for i in range (0, next_layer_size):
-        print("Next layer to I's ")
-        print(i)
-        print("th row: ")
-        print(w_next_layer_toI[i,:])
-        res += (delta_next_layer*w_next_layer_toI[i,:])
-    return siggy*res
+def delta_hlayer(pot_vector_m, delta_next_layer, w_next_layer, next_layer_size, hidden_layer_size):
+    res = []
+    for i in range(0, hidden_layer_size - 1): #ignoring bias again
+        tempRes = delta_hlayer_individual(pot_vector_m[[i],:], delta_next_layer, w_next_layer[:,[i]], next_layer_size)
+        res.append(tempRes)
+    return res
+#step size
+step_size = 0.1
 
-
-#step size 
-step_size = 0.001
-
-#data, data, bias 
-input_data = [[1,1,1],[0,1,1],[1,0,1],[0,0,1]]
-input_vectors =np.matrix(input_data).transpose() 
+#data, data, bias
+input_data = [[1.0,1.0,1.0],[0.0,1.0,1.0],[1.0,0.0,1.0],[0.0,0.0,1.0]]
+input_vectors =np.matrix(input_data).transpose()
 #retrieving column vector: np.matrix(input_data).transpose()[:, [0]]
 
 #labels
@@ -85,50 +87,59 @@ output_label = [0,1,1,0]
 # MLP Structure
 bias = 1
 n = 0
-input_layer = input_vectors[:, [n]]
+
 
 #Weight matrices
 W1 = np.matrix(np.random.rand(2,3))
 W2 = np.matrix(np.random.rand(1,3))
+for notusedbefore in range(0, 10000):
+    for n in range(0, 4):
+        input_layer = input_vectors[:, [n]]
+        #calculating activation vector x1
+        hidden_layer = forward_pass_hidden_layer(W1, input_layer, len(input_layer))
 
-#calculating activation vector x1
-hidden_layer = forward_pass_hidden_layer(W1, input_layer, len(input_layer))
-hidden_layer_potential = x_potential(W1, input_layer, len(input_layer))
-output_layer = forward_pass_output(W2, hidden_layer)
-output_layer_potential = x_potential(W2, hidden_layer, len(hidden_layer))
+        hidden_layer_potential = x_potential(W1, input_layer, len(input_layer))
+        output_layer = forward_pass_output(W2, hidden_layer)
+        output_layer_potential = x_potential(W2, hidden_layer, len(hidden_layer))
 
-print(hidden_layer)
-print(hidden_layer_potential)
-print(output_layer)
-print(output_layer_potential)
+        delta_k = delta_output(output_layer, output_label[n])
+        #since delta_k is a single value, we change that to scalar.
+        delta_ks = np.asscalar(delta_k)
 
-delta_k = delta_output(output_layer, output_label[n])
-#since delta_k is a single value, we change that to scalar.
-delta_ks = np.asscalar(delta_k)
+        bp_deriv1 = loss_derived_wrt_wmij(delta_ks, hidden_layer).transpose()
 
-bp_deriv1 = loss_derived_wrt_wmij(delta_ks, hidden_layer)
+        delta_h = delta_hlayer(hidden_layer_potential, delta_ks, W2, len(output_layer), len(hidden_layer))
 
-delta_1 = delta_hlayer(output_layer_potential, delta_ks, W2, 1)
+        bp_deriv2 = np.array([])
+        for i in range(0, len(delta_h)):
+            for j in range(0, len(input_layer)):
+                bp_deriv2 = np.append(bp_deriv2, np.array([delta_h[i] * W1[i, j]]))
 
-bp_deriv2 = loss_derived_wrt_wmij(delta_1, input_layer)
+        bp_deriv2.shape = (2,3)
+        bp_deriv2 = np.matrix(bp_deriv2)
 
+        w2pre = LA.norm(W2)
+        w1pre = LA.norm(W1)
+        W2 = update_model(W2, step_size, bp_deriv1)
+        W1 = update_model(W1, step_size, bp_deriv2)
+        print(delta_k)
 '''
-#To do: 
+#To do:
 1. Write the weight update function for one neuron. {DONE} See if it can be reliably computed with different variables (eqn 53)
 2. Weight update function should use the BP functions already defined
 3. Numpy nightmare
-4. Bias is always 1. it's the weights that change. 
-5. Step size manipulation? Usually larger is used in the beginning, and smaller at the later epochs 
+4. Bias is always 1. it's the weights that change.
+5. Step size manipulation? Usually larger is used in the beginning, and smaller at the later epochs
 
 a. There are 3 deltas, 2 hidden and 1 output
 b. There are 6 derived values, permutation of UNIT TO UNIT combination between each layer:
-    input layer has 2 units (excl bias). 
+    input layer has 2 units (excl bias).
     hidden layer has 2 units (excl bias).
     output layer has 1 unit.
-    2x2 + 2x1 = 6. 
-    Matter of reshuffling the equations to calculate only the ones that matter, i.e. anything that's NOT weight.  
+    2x2 + 2x1 = 6.
+    Matter of reshuffling the equations to calculate only the ones that matter, i.e. anything that's NOT weight.
 
-Step 0: Initialize weight matrix W1 (3x3) and W2 (3x1) with small random values 
+Step 0: Initialize weight matrix W1 (3x3) and W2 (3x1) with small random values
 Step 1: Start backprop on existing set.
     For i = 0 to and including bias (if it exists), repeat:
         1.1: forward pass: eq. 47, 48, 55 -- potential error source
@@ -137,7 +148,6 @@ Step 1: Start backprop on existing set.
         1.4: Put the values into the corresponding matrix position (fill up the row first)
 Step 2: Run eq. 53 with the new weight matrices and the old weight matrices
 Step 3. Repeat 1 and 2 until threshold has been reached (found in page 81) or n number of repeats have been completed
-    # Changing the step size at each iteration might be wise. 
-Step 4. Return the newest weight vector. 
+    # Changing the step size at each iteration might be wise.
+Step 4. Return the newest weight vector.
 '''
-
