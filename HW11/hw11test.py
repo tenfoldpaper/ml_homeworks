8306826c87de84c12ae8d52ab61ab26456822195
tenfoldpaper <s.bien@jacobs-university.de>
2018-05-26 01:06:28 +0200

    Added plotting, run it with python3

--- HW11/hw11test.py
+++ HW11/hw11test.py
@@ -1,12 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-f = open('results.txt', 'w')
 # sigmoid
 def log_sig(a):
     return 1/(1+np.exp(-a))
 
 def log_sig_derived(a):
     return log_sig(a)*(1-log_sig(a))
+
+def output_process(x):
+    if(x<=0.5):
+        return 0
+    else:
+        return 1
+
+def process_procerror(p):
+    if(p>1):
+        return 1
+    else:
+        return p
 
 #Returns a function vectorizedSigmoid which takes np arrays in
 #and returns nparrays too
@@ -41,12 +52,14 @@ W2 = np.random.random((3, 1))
 
 X = [[0,0],[1,0],[0,1],[1,1]]
 Y = [0, 1, 1, 0]
-epochs = 10000
+epochs = 5000
 error = []
+procerror = []
 for x in range(epochs):
-    if x % 10 == 0:
+    if x % 1 == 0:
         #print "Epochs: %d" % x
         errorval = 0
+        procerrorval = 0
         for i in range(4):
         #Forward pass to test values now
             W1temp = W1
@@ -56,9 +69,13 @@ for x in range(epochs):
             l2 = np.c_[np.ones(1), vectorizedSigmoid(l1.dot(W1temp))]
             l3 = log_sig(l2.dot(W2temp))
             l3cpy = l3
+            l3proc = l3cpy
+            
             #print "%d XOR %d = %.7f" % (l1[0,1], l1[0,2], l3[0,0])
+            procerrorval += np.abs(Y[i] - output_process(l3proc[0,0]))
             errorval += np.abs(Y[i] - l3cpy[0,0])
         error.append(errorval/2)
+        procerror.append(process_procerror(procerrorval/2))
     for i in range(4):
         x = X[i]
         y = Y[i]
@@ -71,12 +88,23 @@ for i in range(4):
     l1 = np.c_[np.ones(1), l1]
     l2 = np.c_[np.ones(1), vectorizedSigmoid(l1.dot(W1))]
     l3 = log_sig(l2.dot(W2))
-    print "%d XOR %d = %.7f" % (l1[0,1], l1[0,2], l3[0,0])
+    print ("%d XOR %d = %.7f" % (l1[0,1], l1[0,2], l3[0,0]))
 
-xvals = range(1, 1001)
+xvals = range(1, 5001)
+
+plt.figure(1)
+plt.subplot(211)
 plt.plot(xvals, error)
-plt.xlabel('epochs * 10')
-plt.ylabel('Average error % ')
-plt.title('Error rate after n*10 epochs')
+plt.xlabel('Epochs', fontsize='smaller')
+plt.ylabel('Average error rate', fontsize='smaller')
+plt.title('Average Error Rate at n Epochs with Unprocessed Output')
+
+plt.subplot(212)
+plt.plot(xvals, procerror)
+plt.xlabel('Epochs',  fontsize='smaller')
+plt.ylabel('Average error rate', fontsize='smaller')
+plt.title('Average Error Rate at n Epochs with Processed Output')
+plt.subplots_adjust(hspace=0.5)
 plt.savefig("res.png")
+
 plt.show()
